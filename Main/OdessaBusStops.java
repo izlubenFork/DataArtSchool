35b65721078ab49df223c23cbb2988d7dbcb0281
StasMenshykov <izluben@yandex.ru>
2013-11-04 02:42:38 +0200

    Enumeration of Odessa`s bus stops was added

---
+++ Main/OdessaBusStops.java
@@ -0,0 +1,21 @@
+package Main;
+
+public enum OdessaBusStops {
+    RailwayStationR,
+    RailwayStationL,
+    MalayaArnautskayaR,
+    MalayaArnautskayaL,
+    BolshayaArnautskayaR,
+    BolshayaArnautskayaL,
+    BazarnayaR,
+    BazarnayaL,
+    UspenskayaR,
+    UspenskayaL,
+    TroitskayaR,
+    TroitskayaL,
+    EkaterininskayaR,
+    EkaterininskayaL,
+    AvtostantsiaR,
+    AvtostantsiaL
+
+}
